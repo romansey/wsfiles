93d2f79e7d274daf32fbd13052aab2fb47048bc5
Roman Seyffarth <roman.seyffarth@twt.de>
2018-07-27 16:10:50 +0200

    initial commit

---
+++ zsh/init.zsh
@@ -0,0 +1,7 @@
+cd "$(dirname "$0")"
+
+source ./zsh-autosuggestions/zsh-autosuggestions.zsh
+source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+source ./custom/aliases.zsh
+source ./custom/prompt.zsh
