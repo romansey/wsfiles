9f111362bba2de86450f93dfcbc2efc0f0f0107e
Roman Seyffarth <roman@seyffarth.one>
2018-08-03 19:30:46 +0200

    added git mass commands

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -8,8 +8,8 @@ fi
 alias l='ls -lh'
 alias ll='ls -lah'
 
-alias editws="$EDITOR $WSFILES"
-alias cdws="cd $WSFILES"
+alias wscd="cd $WSFILES"
+alias wsrl='source ~/.zshrc'
 
 alias pyenv='source ./env/bin/activate'
 
@@ -17,6 +17,7 @@ alias gco='git checkout'
 alias gcot='git checkout -t'
 alias gcob='git checkout -b'
 alias gf='git fetch --all --prune'
+alias gfa='git_fetch_all && git_scan_directory'
 alias gbd='git branch -d'
 alias gclean='git clean -df'
 alias gclear='git reset --hard HEAD && git clean -xdf'
@@ -24,6 +25,7 @@ alias grh='git reset --hard HEAD'
 alias glog='git log --all --graph'
 alias gd='git diff'
 alias gs='git status'
+alias gscan='git_scan_directory'
 alias ga='git add'
 alias gaa='git add -A'
 alias gcam='git commit -am'
--- zsh/custom/functions.zsh
+++ zsh/custom/functions.zsh
@@ -25,3 +25,35 @@ git_current_branch() {
     fi
     echo ${ref#refs/heads/}
 }
+
+# Performs a fetch for all Git repo directories in the current dir
+git_fetch_all() {
+    for dir in ./*/; do
+        if [[ -d "$dir/.git" ]]; then
+            echo "Fetching repo at $dir ..."
+            (cd "$dir" && git fetch --all --prune)
+        fi
+    done
+}
+
+# Outputs the Git status for all subdirectories
+git_scan_directory() {
+    for dir in ./*/; do
+        local branch="$(cd "$dir" && git_current_branch)"
+        local statusSymbols=""
+        if [[ -n $branch ]]; then
+            statusSymbols="$(cd "$dir" && git_status)"
+            if [[ -n $statusSymbols ]]; then
+                if [[ $statusSymbols =~ .*[±?].* ]]; then
+                    echo "\033[0;31m$dir [$branch $statusSymbols]\033[0m"
+                else
+                    echo "\033[0;33m$dir [$branch $statusSymbols]\033[0m"
+                fi
+            else
+                echo "\033[0;32m$dir [$branch ✔]\033[0m"
+            fi
+        else
+            echo "$dir [no git repo]"
+        fi
+    done
+}
